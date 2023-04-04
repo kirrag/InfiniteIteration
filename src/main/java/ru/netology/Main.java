2045dc43a727978ce8a701e563abf92cede6efe8
Garrik Ten <ten@otux.ru>
2023-04-04 23:44:38 +0300

    InfiniteIteration v1.1

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -2,7 +2,8 @@ package ru.netology;
 
 public class Main {
 	public static void main(String[] args) {
-		for (int r : new Randoms(90, 100)) {
+		Randoms randoms = new Randoms(90, 100);
+		for (int r : randoms) {
 			System.out.println("Случайное число: " + r);
 			if (r == 100) {
 				System.out.println("Выпало число 100, давайте на этом закончим");
